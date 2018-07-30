e7a0b3030a3e3bad1d5c14fbc33522a37c01e342
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2018-07-30 10:49:27 +0200

    refactor: add BrowserRouter for routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import Home from './screens/Home';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter
+        basename={process.env.PUBLIC_URL}
+    >
+        <Home />
+    </BrowserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
