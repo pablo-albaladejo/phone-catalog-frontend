4520d92b942c238ffaf4539c399c5297abe5c54e
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2018-07-31 15:01:43 +0200

    test: add test files for routing

---
+++ tests/e2e/routing.test.ts
@@ -0,0 +1,18 @@
+import { Selector, ClientFunction } from "testcafe";
+
+const getWindowLocation = ClientFunction(() => window.location.href);
+
+fixture`Page doesn't exit`.page`http://localhost:3000/not/exits`;
+test("Not existing page redirects to 404", async t => {
+  await t.expect(getWindowLocation()).eql('http://localhost:3000/error/notfound');
+});
+
+fixture`Phone doesn't exit`.page`http://localhost:3000/phone/invalid`;
+test("Not invalid phone id redirects to 404", async t => {
+  await t.expect(getWindowLocation()).eql('http://localhost:3000/error/notfound');
+});
+
+fixture`No phone id provided`.page`http://localhost:3000/phone`;
+test("Not phone id provided redirects to 404", async t => {
+  await t.expect(getWindowLocation()).eql('http://localhost:3000/error/notfound');
+});
