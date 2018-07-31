3e61bb650f742af4b7c7113d733abb2975b65724
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2018-07-31 19:53:40 +0200

    test: typo at test description

--- tests/e2e/routing.test.ts
+++ tests/e2e/routing.test.ts
@@ -3,7 +3,7 @@ import { Selector, ClientFunction } from "testcafe";
 const getWindowLocation = ClientFunction(() => window.location.href);
 
 fixture`Page doesn't exit`.page`http://localhost:3000/not/exits`;
-test("Not existing page redirects to 404", async t => {
+test("No existing page redirects to 404", async t => {
   await t.expect(getWindowLocation()).eql('http://localhost:3000/error/notfound');
 });
 
@@ -13,6 +13,6 @@ test("Invalid phone id redirects to 404", async t => {
 });
 
 fixture`No phone id provided`.page`http://localhost:3000/phone`;
-test("Not phone id provided redirects to 404", async t => {
+test("No phone id provided redirects to 404", async t => {
   await t.expect(getWindowLocation()).eql('http://localhost:3000/error/notfound');
 });
