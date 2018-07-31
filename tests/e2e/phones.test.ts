bdb9cf8ccb73bd70ef590fb7dda4dfea0f05f3b2
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2018-08-01 01:48:46 +0200

    test: update testing url

--- tests/e2e/phones.test.ts
+++ tests/e2e/phones.test.ts
@@ -3,7 +3,7 @@ import { Selector, ClientFunction } from "testcafe";
 // See: http://devexpress.github.io/testcafe/documentation/test-api/obtaining-data-from-the-client.html#executing-client-functions
 const getWindowLocation = ClientFunction(() => window.location.href);
 
-fixture`Phones page`.page`http://localhost:3000/`;
+fixture`Phones page`.page`http://localhost:3000/#/`;
 
 test("A phones list is displayed", async t => {
     const phonesList = await Selector(".phone-preview");
@@ -15,5 +15,5 @@ test("Clicking a phone preview card takes you to the detailed view", async t => 
   const phonesList = await Selector(".phone-preview");
   const viewDetailButton = await phonesList.nth(0).find(".view-detail-button");
   await t.click(viewDetailButton);
-  await t.expect(getWindowLocation()).match(/localhost:3000\/phone\/\d+/);
+  await t.expect(getWindowLocation()).match(/localhost:3000\/#\/phone\/\d+/);
 });
--- tests/e2e/routing.test.ts
+++ tests/e2e/routing.test.ts
@@ -2,17 +2,17 @@ import { Selector, ClientFunction } from "testcafe";
 
 const getWindowLocation = ClientFunction(() => window.location.href);
 
-fixture`Page doesn't exit`.page`http://localhost:3000/not/exits`;
+fixture`Page doesn't exit`.page`http://localhost:3000/#/not/exits`;
 test("No existing page redirects to 404", async t => {
-  await t.expect(getWindowLocation()).eql('http://localhost:3000/error/notfound');
+  await t.expect(getWindowLocation()).eql('http://localhost:3000/#/error/notfound');
 });
 
-fixture`Phone doesn't exit`.page`http://localhost:3000/phone/invalid`;
+fixture`Phone doesn't exit`.page`http://localhost:3000/#/phone/invalid`;
 test("Invalid phone id redirects to 404", async t => {
-  await t.expect(getWindowLocation()).eql('http://localhost:3000/error/notfound');
+  await t.expect(getWindowLocation()).eql('http://localhost:3000/#/error/notfound');
 });
 
-fixture`No phone id provided`.page`http://localhost:3000/phone`;
+fixture`No phone id provided`.page`http://localhost:3000/#/phone`;
 test("No phone id provided redirects to 404", async t => {
-  await t.expect(getWindowLocation()).eql('http://localhost:3000/error/notfound');
+  await t.expect(getWindowLocation()).eql('http://localhost:3000/#/error/notfound');
 });
